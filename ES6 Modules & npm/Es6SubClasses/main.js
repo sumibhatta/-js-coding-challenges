ef4ef4f127e74083f70ba482fe490a34a55d5956
sumibhatta <sumibhatta37@gmail.com>
2025-11-25 07:36:36 +0545

    main fixed for es6 q2

--- ES6 Modules & npm/Es6SubClasses/main.js
+++ ES6 Modules & npm/Es6SubClasses/main.js
@@ -1,5 +1,10 @@
 import { add, sub } from './math.js';
+import g from './greet.js';
 
 export function mathFuns() {
     return `Add: ${add(5, 3)}, Sub: ${sub(5, 3)}`;
+}
+
+export function greetFuns() {
+    return g("Sumi");
 }
